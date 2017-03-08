77b2eceb5eea4233cb0bc5ed77eb7685dc4152db
Eazey <eazey@foxmail.com>
2017-03-08 13:33:30 +0800

    Create MyClass.cs

---
+++ Reflection/Assem/MyClass.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    class MyClass
+    {
+        public int number;
+        
+        public void MyMethod(){}
+    }
+}
