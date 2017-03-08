da9752ef9564feef0ac2fef0c2ca753b7a8f68ca
Eazey <eazey@foxmail.com>
2017-03-08 11:24:50 +0800

    Update Program.cs

--- Reflection/Type/Program.cs
+++ Reflection/Type/Program.cs
@@ -43,27 +43,4 @@ namespace CSharpTest
             Console.WriteLine();
         }
     }
-
-    public class MyClass
-    {
-        //定义属性
-        public string Name { get;private set;}
-        public int Age { get; private set;}
-        //定义字段
-        public string Habit;
-        public int Grade;
-
-        // 构造方法
-        public MyClass(string name, int age)
-        {
-            Name = name;
-            Age = age;
-        }
-
-        //成员方法
-        public void MyMethod1() { }
-        public void MyMethod2() { }
-
-        public static void MyStaticMethod(){ }
-    }
 }
