2492a16ce1e8f0921ff7ed82a957e263c4612cd3
Eazey <eazey@foxmail.com>
2017-03-08 12:02:58 +0800

    Update MyClass.cs

--- Reflection/Type/MyClass.cs
+++ Reflection/Type/MyClass.cs
@@ -9,23 +9,35 @@ namespace CSharpTest
     public class MyClass
     {
         //定义属性
-        public string Name { get; private set; }
-        public int Age { get; private set; }
+        public string Name
+        {
+            get { return name; }
+        }
+        public int Age 
+        {
+            get { return age; }
+        }
+        private float height { get; set; }
+
         //定义字段
         public string Habit;
         public int Grade;
+        private string name;
+        private int age;
 
         // 构造方法
         public MyClass(string name, int age)
         {
-            Name = name;
-            Age = age;
+            this.name = name;
+            this.age = age;
         }
 
         //成员方法
         public void MyMethod1() { }
         public void MyMethod2() { }
+        private void MyMethod3() { }
 
-        public static void MyStaticMethod() { }
+        public static void MyStaticMethod1() { }
+        private static void MyStaticMethod2() { }
     }
 }
